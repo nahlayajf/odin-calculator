eed57e469ebbd806e1e2be9aba207e041c8204b2
nahlayajf <06639425+nahlayajf@users.noreply.github.com.>
2026-09-06 19:20:55 -0400

    Create functions for math operations

---
+++ script.js
@@ -0,0 +1,36 @@
+function addition(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operator(operator, num1, num2) {
+    switch (operator) {
+        case "add":
+            addition(num1, num2);
+            break;
+        case "subtract":
+            subtract(num1, num2);
+            break;
+        case "multiply":
+            multiply(num1, num2);
+            break;
+        case "divide":
+            divide(num1, num2);
+            break;
+    }
+}
+let num1;
+let num2;
+let operator;
+
